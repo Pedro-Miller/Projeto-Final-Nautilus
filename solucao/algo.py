b3aa28dd1543053654fa09cd9910682265ffa0b5
janot3388 <janotbrillard3388@gmail.com>
2023-03-18 14:23:49 -0300

    +add-nemo

--- solucao/algo.py
+++ solucao/algo.py
@@ -1,90 +1,104 @@
+#!/usr/bin/python3
+
 import rospy
 import tf2_ros as tf
-
-from geometry_msgs.msg import Twist, Pose, Vector3
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist, Pose, Vector3, PointStamped
 import math
+import tf.transformations as tft
 
 
 
+# Callback for sonar data
+def sonar_callback(msg, state):
+    state['sonar_data'] = (msg.point.x, msg.point.y)
 
 
-# Callback sonar
-def sonar_callback(msg):
-    x_m = msg.x
-    y_m = msg.y
-    return (x_m,y_m)
-
-def odom_callback(msg):
-    
-    px = msg.position.x
-    py = msg.position.y
-
-    orientation = msg.orientation
-
-    # Create a tf.Transform object with the Quaternion orientation as the rotation
-    tf_transform = tf.TransformerROS().fromTranslationRotation([0, 0, 0], [orientation.x, orientation.y, orientation.z, orientation.w])
-    # Use the tf.transformations.euler_from_quaternion function to convert the orientation to Euler angles
-    angulos_euler = tf.transformations.euler_from_quaternion(tf_transform.getRotation())
-
-    # Extract the yaw angle from the Euler angles
-    yaw = angulos_euler[2]
-    return (px,py,yaw)
+# Callback for odometry data
+def odom_callback(msg, state):
+    state['odom_data'] = msg.pose.pose
 
 
-def orientar():
-    # calcula diferença de angulo entre carros
-    odom = odom_callback()
-    sonar = sonar_callback()
+# Function to calculate angular velocity required for orientation
+def orientar(state):
+    if 'sonar_data' in state and 'odom_data' in state:
+        sonar_data = state['sonar_data']
+        odom_data = state['odom_data']
 
-    dx = sonar[0] - odom[0]
-    dy = sonar[1] - odom[1]
+        # Extract position and orientation data from the odom message
+        px = odom_data.position.x
+        py = odom_data.position.y
+        orientation = odom_data.orientation
 
-    target_angle = math.atan2(dy, dx)
-    current_angle =  odom[2]
+        # Convert orientation to Euler angles and extract yaw angle
+        quat = [orientation.x, orientation.y, orientation.z, orientation.w]
+        _, _, yaw = tft.euler_from_quaternion(quat)
 
-    angle_erro = target_angle - current_angle
+        # Extract sonar position data
+        x_m= sonar_data[0]
+        y_m= sonar_data[1]
 
-    # normaliza a diferença para -pi e pi
-    while angle_erro > math.pi:
-        angle_erro -= 2 * math.pi
-    while angle_erro < -math.pi:
-        angle_erro += 2 * math.pi
 
-    # retorna velocidade angular necessaria para orientação
-    velang = Vector3(z=angle_erro)
-    return velang  
-    
+        # Calculate target angle using cross product of two vectors
+        target_angle = math.atan2(-y_m, -x_m)
+        aerror = target_angle - yaw
 
-if __name__ == "__main__":
+        rospy.loginfo('yaw:'+ str(yaw) +'tangl' + str(target_angle)+ ' tgangl: '+str(aerror) )
 
+
+
+        # Normalize angle error to -pi to pi range
+        while aerror > math.pi:
+            aerror -= 2 * math.pi
+        while aerror < -math.pi:
+            aerror += 2 * math.pi
+
+
+
+        rospy.loginfo('aer after norm: '+ str(aerror))
+        
+
+        # Calculate angular velocity required for orientation
+        vel_ang = aerror
+
+        return vel_ang
+
+
+def main():
+    # Initialize node
     rospy.init_node("algo")
 
-    rospy.Subscriber("/odom", Pose, odom_callback)
+    # Create state dictionary
+    state = {}
 
+    # Subscribe to sonar and odometry topics
+    rospy.Subscriber("/sonar_data", PointStamped, sonar_callback, state)
+    rospy.Subscriber("/odom", Odometry, odom_callback, state)
 
-    rospy.Subscriber("/sonar_data", Twist, sonar_callback)
-
-
+    # Setup publisher for velocity commands
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(30)
 
     # Main loop
     while not rospy.is_shutdown():
-
         twist = Twist()
 
-        twist.angular = orientar()
-        twist.linear.y = 50
+        # Calculate angular velocity for orientation
+        vel_ang = orientar(state)
 
+        # Set angular velocity in twist message
+        if vel_ang is not None:
+            twist.angular.z = vel_ang
+
+        # Set linear velocity in twist message
+        twist.linear.y = 0.5
+
+        # Publish twist message
         pub.publish(twist)
 
         rate.sleep()
 
 
-
-
-    
-
-
-
+if __name__ == "__main__":
+    main()
